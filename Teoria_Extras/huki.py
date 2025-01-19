6626660e4caab66dbb6429e50c6e4ab462933cb5
Elias Mina <hukitan@hotmail.com>
2025-01-19 13:05:31 -0600

    Se mejora la función de análisis de datos para manejar conjuntos pequeños y se corrigen descripciones

--- Teoria_Extras/huki.py
+++ Teoria_Extras/huki.py
@@ -1,13 +1,18 @@
 def looks(datos):
-    """Genera un analisis de todo el set de datos de manera gloabal"""
-    print("Se ejecuta la descripcion de los datos:")
-    print(datos.describe(include= "all"))
+    
+    """Genera un análisis de todo el set de datos de manera global"""
+
+    print("Se ejecuta la descripción de los datos:")
+    print(datos.describe(include="all"))
     print()
-    print("se hace exploracion con metodo info:")
-    datos.info()
+    print("se hace exploración con método info:")
+    print(datos.info())
     print()
-    print("se hace una observacion con el metodo Head:")
-    print(datos.head())
+    print("se hace una observación con el método Sample:")
+    if len(datos) > 10:
+        print(datos.sample(10))
+    else:
+        print(datos)
     print()
     print("hay NA?:")
     print(datos.isna().sum())
